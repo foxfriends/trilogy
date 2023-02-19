00640cad6cc1a4a811b045682f5336c596f115b0
Cameron Eldridge <cameldridge@gmail.com>
2023-02-18 22:20:49 -0500

    boolean query tests

--- trilogy-parser/src/syntax/boolean_query.rs
+++ trilogy-parser/src/syntax/boolean_query.rs
@@ -10,8 +10,23 @@ pub struct BooleanQuery {
 
 impl BooleanQuery {
     pub(crate) fn parse(parser: &mut Parser) -> SyntaxResult<Self> {
-        let start = parser.expect(KwIs).expect("Caller should have found this");
+        let start = parser
+            .expect(KwIs)
+            .map_err(|token| parser.expected(token, "expected `is`"))?;
         let expression = Expression::parse_parameter_list(parser)?; // this isn't a parameter list, but we don't allow commas
         Ok(Self { start, expression })
     }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    test_parse!(bool_query_simple: "is true" => BooleanQuery::parse => "(BooleanQuery _)");
+    test_parse!(bool_query_expression: "is x < 5" => BooleanQuery::parse => "(BooleanQuery _)");
+    test_parse!(bool_query_application: "is f x y" => BooleanQuery::parse => "(BooleanQuery _)");
+    test_parse_error!(bool_query_commas: "is x, x" => BooleanQuery::parse);
+    test_parse!(bool_query_commas_parens: "is (x, x)" => BooleanQuery::parse => "(BooleanQuery _)");
+    test_parse_error!(bool_query_no_is: "(x, x)" => BooleanQuery::parse => "expected `is`");
+    test_parse_error!(bool_query_invalid_expr: "is { let x = 5 }" => BooleanQuery::parse);
 }
