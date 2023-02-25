1d6a3bd6bf0302fe808cf8a48f50ca37b2602675
Cameron Eldridge <cameldridge@gmail.com>
2023-02-24 21:53:25 -0500

    tiny tidying

--- trilogy-lexical-ir/src/code.rs
+++ trilogy-lexical-ir/src/code.rs
@@ -2,7 +2,13 @@ use super::*;
 
 #[derive(Clone, Debug)]
 pub enum Code {
+    // Grouping
     Scope(Box<Scope>),
+
+    // Primitive operations
+    // TODO: how to do these...
+
+    // Three kinds of control flow:
     Loop(Vec<Branch>),   // `for` and `while`
     Branch(Vec<Branch>), // `if` and `match
     Handle(Vec<Branch>), // `when` and `given`
