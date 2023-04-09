a2510b243fc84c5fe77e3d0a89f464c2bce88111
Cameron Eldridge <cameldridge@gmail.com>
2023-04-09 15:21:25 -0400

    Improving `dev scan` command a bit

--- trilogy/src/dev.rs
+++ trilogy/src/dev.rs
@@ -1,13 +1,18 @@
 use clap::Subcommand;
+use colored::*;
 use pretty::{DocAllocator, RcAllocator};
 use std::path::PathBuf;
 use trilogy_parser::{Parser, PrettyPrintSExpr};
-use trilogy_scanner::Scanner;
+use trilogy_scanner::{Scanner, TokenType, TokenValue};
 
 #[derive(Subcommand, Clone, Debug)]
 pub enum Command {
     /// Scan a file, printing out its tokens.
-    Scan { file: PathBuf },
+    Scan {
+        file: PathBuf,
+        #[arg(short, long)]
+        debug: bool,
+    },
     /// Parse a file, printing out its AST.
     Parse {
         file: PathBuf,
@@ -20,11 +25,38 @@ pub enum Command {
 
 pub fn run(command: Command) -> std::io::Result<()> {
     match command {
-        Command::Scan { file } => {
+        Command::Scan { file, debug } => {
             let contents = std::fs::read_to_string(file)?;
             let scanner = Scanner::new(&contents);
             for token in scanner {
-                println!("{token:?}");
+                if debug {
+                    println!("{token:?}");
+                    continue;
+                }
+                print!("{}", token.span.to_string().cyan());
+                print!("   {}", format!("{:?}", token.token_type).yellow());
+                match token.token_type {
+                    TokenType::Identifier => {
+                        let Some(TokenValue::String(value)) = token.value else { unreachable!() };
+                        print!(" ({})", value.blue());
+                    }
+                    TokenType::Numeric
+                    | TokenType::Bits
+                    | TokenType::Character
+                    | TokenType::String => match token.value {
+                        Some(TokenValue::Bits(bits)) => {
+                            print!(" ({})", bits.to_string().bright_yellow())
+                        }
+                        Some(TokenValue::String(s)) => print!(" ({})", s.green()),
+                        Some(TokenValue::Char(ch)) => print!(" ({})", ch.to_string().green()),
+                        Some(TokenValue::Number(n)) => {
+                            print!(" ({})", n.to_string().bright_yellow())
+                        }
+                        _ => unreachable!(),
+                    },
+                    _ => {}
+                }
+                println!();
             }
         }
         Command::Parse {
