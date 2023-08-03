84016dbe5cd14cc9d4492c377b98aadb026c8cd4
Cameron Eldridge <cameldridge@gmail.com>
2023-08-03 09:12:47 -0400

    query bug fix

--- trilogy-codegen/src/query.rs
+++ trilogy-codegen/src/query.rs
@@ -19,22 +19,10 @@ pub(crate) fn write_query_state(context: &mut Context, query: &ir::Query) {
                 .write_instruction(Instruction::Swap)
                 .write_instruction(Instruction::Call(1));
         }
-        ir::QueryValue::Alternative(alt) => {
-            write_query_state(context, &alt.0);
-            write_query_state(context, &alt.1);
-            context.write_instruction(Instruction::Cons);
-        }
-        ir::QueryValue::Conjunction(alt) => {
-            write_query_state(context, &alt.0);
-            write_query_state(context, &alt.1);
-            context.write_instruction(Instruction::Cons);
-        }
-        ir::QueryValue::Disjunction(alt) => {
-            write_query_state(context, &alt.0);
-            write_query_state(context, &alt.1);
-            context.write_instruction(Instruction::Cons);
-        }
-        ir::QueryValue::Implication(alt) => {
+        ir::QueryValue::Alternative(alt)
+        | ir::QueryValue::Conjunction(alt)
+        | ir::QueryValue::Implication(alt)
+        | ir::QueryValue::Disjunction(alt) => {
             write_query_state(context, &alt.0);
             write_query_state(context, &alt.1);
             context.write_instruction(Instruction::Cons);
@@ -47,8 +35,8 @@ pub(crate) fn write_query_state(context: &mut Context, query: &ir::Query) {
 /// Upon finding some solution, the bindings have been set, and the new state is left on
 /// the top of the stack.
 ///
-/// On failure to find another solution, jump to `on_fail` with the state value
-/// having been consumed.
+/// On failure to find another solution, jump to `on_fail` with the state value left so
+/// that all further requests will also fail.
 ///
 /// It's up to the user of the query to call `write_query_state` to ensure that the
 /// initial state is set, as well as to ensure that the state value is carried around
@@ -57,8 +45,9 @@ pub(crate) fn write_query(context: &mut Context, query: &ir::Query, on_fail: &st
     match &query.value {
         ir::QueryValue::Direct(unification) => {
             context
-                .cond_jump(on_fail)
-                .write_instruction(Instruction::Const(false.into()));
+                .write_instruction(Instruction::Const(false.into()))
+                .write_instruction(Instruction::Swap)
+                .cond_jump(on_fail);
             write_expression(context, &unification.expression);
             write_pattern_match(context, &unification.pattern, on_fail);
         }
@@ -76,7 +65,7 @@ pub(crate) fn write_query(context: &mut Context, query: &ir::Query, on_fail: &st
                 .write_instruction(Instruction::Copy)
                 .write_instruction(Instruction::Const(done.into()))
                 .write_instruction(Instruction::ValNeq)
-                .cond_jump(on_fail)
+                .cond_jump(&cleanup)
                 // Runtime type error is probably expected if it's not an iterator when an
                 // iterator is expected, but we just go to  fail instead anyway because it
                 // seems easier for now. Maybe come back to that later, a panic instruction
@@ -101,8 +90,9 @@ pub(crate) fn write_query(context: &mut Context, query: &ir::Query, on_fail: &st
         }
         ir::QueryValue::Is(expr) => {
             context
-                .cond_jump(on_fail)
-                .write_instruction(Instruction::Const(false.into()));
+                .write_instruction(Instruction::Const(false.into()))
+                .write_instruction(Instruction::Swap)
+                .cond_jump(on_fail);
             write_expression(context, expr);
             context.cond_jump(on_fail);
         }
@@ -111,9 +101,62 @@ pub(crate) fn write_query(context: &mut Context, query: &ir::Query, on_fail: &st
         }
         ir::QueryValue::Pass => {
             context
-                .cond_jump(on_fail)
-                .write_instruction(Instruction::Const(false.into()));
+                .write_instruction(Instruction::Const(false.into()))
+                .write_instruction(Instruction::Swap)
+                .cond_jump(on_fail);
         }
+        // ir::QueryValue::Conjunction(conj) => {
+        //     let cleanup = context.labeler.unique_hint("conj_cleanup");
+        //     let done = context.labeler.unique_hint("conj_done");
+        //     let next = context.labeler.unique_hint("conj_next");
+        //     let retry = context.labeler.unique_hint("conj");
+        //     context
+        //         .write_instruction(Instruction::Uncons)
+        //         .write_label(retry.clone())
+        //         .write_instruction(Instruction::Swap);
+        //     context.scope.intermediate();
+        //     write_query(context, &conj.0, &cleanup);
+        //     context.write_instruction(Instruction::Swap);
+        //     write_query(context, &conj.1, &next);
+        //     context.scope.end_intermediate();
+        //     context
+        //         .write_instruction(Instruction::Cons)
+        //         .jump(&done)
+        //         .write_label(next)
+        //         .write_instruction(Instruction::Pop);
+        //     write_query_state(context, &conj.1);
+        //     context.jump(&retry);
+        //     context
+        //         .write_label(cleanup)
+        //         .write_instruction(Instruction::Pop)
+        //         .jump(on_fail)
+        //         .write_label(done);
+        // }
+        // ir::QueryValue::Disjunction(disj) => {
+        //     let cleanup = context.labeler.unique_hint("disj_cleanup");
+        //     let next = context.labeler.unique_hint("disj_next");
+        //     let done = context.labeler.unique_hint("disj_done");
+        //     context
+        //         .write_instruction(Instruction::Uncons)
+        //         .write_instruction(Instruction::Swap);
+        //     context.scope.intermediate();
+        //     write_query(context, &disj.0, &next);
+        //     context
+        //         .write_instruction(Instruction::Swap)
+        //         .write_instruction(Instruction::Cons)
+        //         .jump(&done)
+        //         .write_label(next)
+        //         .write_instruction(Instruction::Swap);
+        //     write_query(context, &disj.1, &cleanup);
+        //     context.scope.end_intermediate();
+        //     context
+        //         .write_instruction(Instruction::Cons)
+        //         .jump(&done)
+        //         .write_label(cleanup)
+        //         .write_instruction(Instruction::Pop)
+        //         .jump(on_fail)
+        //         .write_label(done);
+        // }
         value => todo!("{value:?}"),
     }
 }
