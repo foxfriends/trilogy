c5354ab359ea4d29e561cd3150f6b9136dcfda4f
Cameron Eldridge <cameldridge@gmail.com>
2023-10-01 13:36:18 -0400

    tests for the location stuff, fixing many issues

--- trilogy/src/location.rs
+++ trilogy/src/location.rs
@@ -15,7 +15,7 @@ impl Location {
     }
 
     pub(crate) fn absolute(url: Url) -> Self {
-        Self(url)
+        Self::from(url)
     }
 
     pub(crate) fn entrypoint(root_dir: PathBuf, file: impl AsRef<Path>) -> Self {
@@ -23,12 +23,12 @@ impl Location {
     }
 
     pub(crate) fn local_absolute(path: impl AsRef<Path>) -> Self {
-        Self(Url::from_file_path(path).unwrap())
+        Self::from(Url::from_file_path(path).unwrap())
     }
 
     // TODO: this should probably not be unwrapping so liberally
     pub(crate) fn relative(&self, path: &str) -> Self {
-        let mut url = match path.parse::<Url>() {
+        let url = match path.parse::<Url>() {
             Ok(mut url) if url.scheme() == "file" => {
                 url.set_scheme(self.0.scheme()).unwrap();
                 url
@@ -36,24 +36,32 @@ impl Location {
             Ok(url) => url,
             Err(..) => self.0.join(path.as_ref()).unwrap(),
         };
+        Self::from(url)
+    }
+}
+
+impl From<Url> for Location {
+    fn from(mut url: Url) -> Self {
         let mut skip = 0;
-        let mut segments = vec![];
-        for segment in url.path_segments().unwrap().rev() {
-            if segment == ".." {
-                skip += 1;
-                continue;
+        if let Some(path_segments) = url.path_segments() {
+            let mut segments = vec![];
+            for segment in path_segments.rev() {
+                if segment == ".." {
+                    skip += 1;
+                    continue;
+                }
+                if segment == "." {
+                    continue;
+                }
+                if skip > 0 {
+                    skip -= 1;
+                    continue;
+                }
+                segments.push(segment);
             }
-            if segment == "." {
-                continue;
-            }
-            if skip > 0 {
-                skip -= 1;
-                continue;
-            }
-            segments.push(segment);
+            segments.reverse();
+            url.set_path(&segments.join("/"));
         }
-        segments.reverse();
-        url.set_path(&segments.join("/"));
         Self(url)
     }
 }
@@ -79,5 +87,62 @@ impl From<Location> for Url {
 impl Display for Location {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.0)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Location;
+    use std::path::PathBuf;
+
+    #[test]
+    fn location_entrypoint() {
+        let location = Location::entrypoint(PathBuf::from("/a/b/c/"), "./d/e/f");
+        assert_eq!(
+            location,
+            Location::absolute("file:///a/b/c/d/e/f".parse().unwrap())
+        );
+    }
+
+    #[test]
+    fn location_entrypoint_resolve() {
+        let location = Location::entrypoint(PathBuf::from("/a/b/c/"), "../../d/e/f");
+        assert_eq!(
+            location,
+            Location::absolute("file:///a/d/e/f".parse().unwrap())
+        );
+    }
+
+    #[test]
+    fn location_absolute_resolve() {
+        let location = Location::absolute("file:///a/b/c/d/../e".parse().unwrap());
+        assert_eq!(
+            location,
+            Location::absolute("file:///a/b/c/e".parse().unwrap())
+        );
+    }
+
+    #[test]
+    fn location_relative() {
+        let location = Location::absolute("file:///a/b/c".parse().unwrap());
+        assert_eq!(
+            location.relative("./d"),
+            Location::absolute("file:///a/b/d".parse().unwrap())
+        );
+    }
+
+    #[test]
+    fn location_relative_resolve() {
+        let location = Location::absolute("file:///a/b/c".parse().unwrap());
+        assert_eq!(
+            location.relative("../d"),
+            Location::absolute("file:///a/d".parse().unwrap())
+        );
+    }
+
+    #[test]
+    fn library() {
+        let location = Location::library("std").unwrap();
+        assert_eq!(location, Location::absolute("trilogy:std".parse().unwrap()));
     }
 }
