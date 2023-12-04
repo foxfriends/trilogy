a3bf30ccc24656bfac50c3aa36a0d95dd2008733
Cameron Eldridge <cameldridge@gmail.com>
2023-12-03 22:29:16 -0500

    is simulating execution to locate optimizations a real strategy?

--- trilogy-vm/src/bytecode/optimization/mod.rs
+++ trilogy-vm/src/bytecode/optimization/mod.rs
@@ -1,9 +1,47 @@
 use super::chunk::Line;
+use crate::Value;
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+enum Type {
+    Unit,
+    Boolean,
+    Character,
+    Number,
+    Bits,
+    Atom,
+    String,
+    Tuple,
+    Array,
+    Record,
+    Set,
+    Struct,
+    Callable,
+}
+
+#[derive(Clone, Eq, PartialEq, Debug)]
+enum CellValue {
+    Constant(Value),
+    Typed(Type),
+    Any,
+    Unknown,
+    Empty,
+}
+
+struct Cell {
+    contents: CellValue,
+}
+
+#[derive(Default)]
+struct Simulator {
+    stack: Vec<Cell>,
+    registers: Vec<Cell>,
+}
 
 pub(super) fn optimize(
     lines: Vec<Line>,
     _entrypoint: usize,
     _force_reachable: &[String],
 ) -> Vec<Line> {
+    let simulator = Simulator::default();
     lines
 }
