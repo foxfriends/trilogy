87543b17e5a5266369b0e32b1860d33410864107
Cameron Eldridge <cameldridge@gmail.com>
2025-01-26 20:57:36 -0500

    just delete this unused for now

--- trilogy-llvm/src/internal.rs
+++ trilogy-llvm/src/internal.rs
@@ -1,5 +1,3 @@
-#![expect(dead_code, reason = "WIP")]
-
 use inkwell::{
     module::Linkage,
     types::FunctionType,
@@ -17,18 +15,6 @@ impl<'ctx> Codegen<'ctx> {
         self.module.add_function(name, ty, Some(Linkage::External))
     }
 
-    /// Untags a unit, though returns nothing. Fairly useless except as an assertion.
-    pub(crate) fn trilogy_unit_untag(&self, value: PointerValue<'ctx>, name: &str) {
-        let f = self.declare_internal(
-            "trilogy_unit_untag",
-            self.context.void_type().fn_type(
-                &[self.context.ptr_type(AddressSpace::default()).into()],
-                false,
-            ),
-        );
-        self.builder.build_call(f, &[value.into()], name).unwrap();
-    }
-
     /// Untags a boolean value. The return value is of type `i1`.
     pub(crate) fn trilogy_boolean_untag(
         &self,
@@ -38,27 +24,6 @@ impl<'ctx> Codegen<'ctx> {
         let f = self.declare_internal(
             "trilogy_boolean_untag",
             self.context.bool_type().fn_type(
-                &[self.context.ptr_type(AddressSpace::default()).into()],
-                false,
-            ),
-        );
-        self.builder
-            .build_call(f, &[value.into()], name)
-            .unwrap()
-            .try_as_basic_value()
-            .unwrap_left()
-            .into_int_value()
-    }
-
-    /// Untags a character value. The return value is of i32 type (a unicode code point).
-    pub(crate) fn trilogy_character_untag(
-        &self,
-        value: PointerValue<'ctx>,
-        name: &str,
-    ) -> IntValue<'ctx> {
-        let f = self.declare_internal(
-            "trilogy_character_untag",
-            self.context.i32_type().fn_type(
                 &[self.context.ptr_type(AddressSpace::default()).into()],
                 false,
             ),
@@ -114,27 +79,6 @@ impl<'ctx> Codegen<'ctx> {
             .unwrap();
     }
 
-    /// Untags a string value. The returned PointerValue points to a value of `string_value_type`.
-    pub(crate) fn trilogy_string_untag(
-        &self,
-        value: PointerValue<'ctx>,
-        name: &str,
-    ) -> PointerValue<'ctx> {
-        let f = self.declare_internal(
-            "trilogy_string_untag",
-            self.context.ptr_type(AddressSpace::default()).fn_type(
-                &[self.context.ptr_type(AddressSpace::default()).into()],
-                false,
-            ),
-        );
-        self.builder
-            .build_call(f, &[value.into()], name)
-            .unwrap()
-            .try_as_basic_value()
-            .unwrap_left()
-            .into_pointer_value()
-    }
-
     /// Initializes an atom value
     pub(crate) fn trilogy_atom_init(&self, target: PointerValue<'ctx>, value: IntValue<'ctx>) {
         let f = self.declare_internal(
@@ -150,107 +94,6 @@ impl<'ctx> Codegen<'ctx> {
         self.builder
             .build_call(f, &[target.into(), value.into()], "")
             .unwrap();
-    }
-
-    /// Untags an atom value.
-    pub(crate) fn trilogy_atom_untag(
-        &self,
-        value: PointerValue<'ctx>,
-        name: &str,
-    ) -> IntValue<'ctx> {
-        let f = self.declare_internal(
-            "trilogy_atom_untag",
-            self.context.i64_type().fn_type(
-                &[self.context.ptr_type(AddressSpace::default()).into()],
-                false,
-            ),
-        );
-        self.builder
-            .build_call(f, &[value.into()], name)
-            .unwrap()
-            .try_as_basic_value()
-            .unwrap_left()
-            .into_int_value()
-    }
-
-    pub(crate) fn trilogy_bits_untag(
-        &self,
-        value: PointerValue<'ctx>,
-        name: &str,
-    ) -> PointerValue<'ctx> {
-        let f = self.declare_internal(
-            "trilogy_bits_untag",
-            self.context.ptr_type(AddressSpace::default()).fn_type(
-                &[self.context.ptr_type(AddressSpace::default()).into()],
-                false,
-            ),
-        );
-        self.builder
-            .build_call(f, &[value.into()], name)
-            .unwrap()
-            .try_as_basic_value()
-            .unwrap_left()
-            .into_pointer_value()
-    }
-
-    pub(crate) fn trilogy_struct_untag(
-        &self,
-        value: PointerValue<'ctx>,
-        name: &str,
-    ) -> PointerValue<'ctx> {
-        let f = self.declare_internal(
-            "trilogy_struct_untag",
-            self.context.ptr_type(AddressSpace::default()).fn_type(
-                &[self.context.ptr_type(AddressSpace::default()).into()],
-                false,
-            ),
-        );
-        self.builder
-            .build_call(f, &[value.into()], name)
-            .unwrap()
-            .try_as_basic_value()
-            .unwrap_left()
-            .into_pointer_value()
-    }
-
-    pub(crate) fn trilogy_tuple_untag(
-        &self,
-        value: PointerValue<'ctx>,
-        name: &str,
-    ) -> PointerValue<'ctx> {
-        let f = self.declare_internal(
-            "trilogy_tuple_untag",
-            self.context.ptr_type(AddressSpace::default()).fn_type(
-                &[self.context.ptr_type(AddressSpace::default()).into()],
-                false,
-            ),
-        );
-        self.builder
-            .build_call(f, &[value.into()], name)
-            .unwrap()
-            .try_as_basic_value()
-            .unwrap_left()
-            .into_pointer_value()
-    }
-
-    pub(crate) fn trilogy_array_untag(
-        &self,
-        value: PointerValue<'ctx>,
-        name: &str,
-    ) -> PointerValue<'ctx> {
-        let f = self.declare_internal(
-            "trilogy_array_untag",
-            self.context.ptr_type(AddressSpace::default()).fn_type(
-                &[self.context.ptr_type(AddressSpace::default()).into()],
-                false,
-            ),
-        );
-        self.builder
-            .build_call(f, &[value.into()], name)
-            .unwrap()
-            .try_as_basic_value()
-            .unwrap_left()
-            .into_pointer_value()
     }
 
     pub(crate) fn trilogy_array_init_cap(
@@ -318,46 +161,6 @@ impl<'ctx> Codegen<'ctx> {
         self.builder
             .build_call(f, &[array.into(), value.into()], "")
             .unwrap();
-    }
-
-    pub(crate) fn trilogy_set_untag(
-        &self,
-        value: PointerValue<'ctx>,
-        name: &str,
-    ) -> PointerValue<'ctx> {
-        let f = self.declare_internal(
-            "trilogy_set_untag",
-            self.context.ptr_type(AddressSpace::default()).fn_type(
-                &[self.context.ptr_type(AddressSpace::default()).into()],
-                false,
-            ),
-        );
-        self.builder
-            .build_call(f, &[value.into()], name)
-            .unwrap()
-            .try_as_basic_value()
-            .unwrap_left()
-            .into_pointer_value()
-    }
-
-    pub(crate) fn trilogy_record_untag(
-        &self,
-        value: PointerValue<'ctx>,
-        name: &str,
-    ) -> PointerValue<'ctx> {
-        let f = self.declare_internal(
-            "trilogy_record_untag",
-            self.context.ptr_type(AddressSpace::default()).fn_type(
-                &[self.context.ptr_type(AddressSpace::default()).into()],
-                false,
-            ),
-        );
-        self.builder
-            .build_call(f, &[value.into()], name)
-            .unwrap()
-            .try_as_basic_value()
-            .unwrap_left()
-            .into_pointer_value()
     }
 
     /// Untags a callable value. The returned PointerValue points to a `trilogy_callable_value`.
