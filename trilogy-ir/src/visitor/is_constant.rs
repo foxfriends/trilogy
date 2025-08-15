a77fbcb2f056c0e01d8b910d3612aac92f1ec3f2
Cameron Eldridge <cameldridge@gmail.com>
2025-08-14 20:11:14 -0400

    better constant reference of non-constant value detection, slightly

--- trilogy-ir/src/visitor/is_constant.rs
+++ trilogy-ir/src/visitor/is_constant.rs
@@ -3,19 +3,32 @@ use crate::ir;
 
 pub struct IsConstant {
     result: bool,
+    is_calling: bool,
 }
 
 impl IsConstant {}
 
 impl IrVisitor for IsConstant {
     fn visit_identifier(&mut self, _value: &ir::Identifier) {
-        self.result = false;
+        if self.is_calling {
+            self.result = false;
+        }
+    }
+
+    fn visit_application(&mut self, node: &ir::Application) {
+        self.is_calling = true;
+        node.function.visit(self);
+        self.is_calling = false;
+        node.argument.visit(self);
     }
 }
 
 pub trait MightBeConstant: IrVisitable + Sized {
     fn is_constant(&self) -> bool {
-        let mut can_evaluate = IsConstant { result: true };
+        let mut can_evaluate = IsConstant {
+            result: true,
+            is_calling: false,
+        };
         self.visit(&mut can_evaluate);
         can_evaluate.result
     }
